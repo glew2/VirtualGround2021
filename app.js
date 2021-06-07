e7bd70968a1845a4b730bc9fd09a7aceb908f03e
glew2 <glewisonrv@gmail.com>
2021-06-06 20:19:10 -0400

    Created new WORKING server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(80);
