c0ba975cbbc022d6a91f47ddc0cd6321301680a8
glew2 <glewisonrv@gmail.com>
2021-06-07 10:39:38 -0400

    Working server 1

--- app.js
+++ app.js
@@ -7,5 +7,22 @@ app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/script.js', function(req, res,next) {
+    res.sendFile(__dirname + '/script.js');
+})
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })
+  })
 
 server.listen(80);
